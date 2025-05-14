26a93129d548f561ced24f83eb71d90ea963e11a
ROSHIII96 <joseth.vargas.venegas@est.una.ac.cr>
2025-05-14 02:48:06 -0600

    CambiosNavbar1.1

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -7,16 +7,14 @@ const Navbar = () => {
   
     return (
         <nav className="flex items-center justify-between bg-gray-100 p-4 shadow-md">
-           {!isAuthenticated && (
-              <Link
-                to="/reportes"
-                className="bg-yellow-600 hover:bg-yellow-500 text-white py-2 px-4 rounded shadow-lg transition duration-300"
-              >
+           
+        <Link to="/" className="text-lg font-semibold text-gray-700 hover:text-blue-500">Home</Link>
+        <Link to="/quienes-somos" className="text-lg font-semibold text-gray-700 hover:text-blue-500">Quienes somos</Link>
+         {!isAuthenticated && (
+              <Link to="/reportes" className="bg-yellow-600 hover:bg-yellow-500 text-white py-2 px-4 rounded shadow-lg transition duration-300">
                 Reportar Avería
               </Link>
             )}
-        <Link to="/" className="text-lg font-semibold text-gray-700 hover:text-blue-500">Home</Link>
-        <Link to="/quienes-somos" className="text-lg font-semibold text-gray-700 hover:text-blue-500">Quienes somos</Link>
          {isAuthenticated && (
         <Link to="/listareportes" className="text-lg font-semibold text-gray-700 hover:text-blue-500">Reportes</Link>
             )}

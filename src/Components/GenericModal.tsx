6a0f80b737450949188023ea3858e5cf0b4d3086
ROSHIII96 <joseth.vargas.venegas@est.una.ac.cr>
2025-06-03 13:35:35 -0600

    Revision 1.2

--- src/Components/GenericModal.tsx
+++ src/Components/GenericModal.tsx
@@ -1,27 +1,25 @@
-
-//import ClienteFormulario from './ClienteFormularioAgregar';
-
-
 const GenericModal = ({ show, onClose, title, children }) => {
 
-    if (!show) return null;
+    if (!show) return null;  //Para que el modal no se muestre de un solo
 
     return (
       // fondo
       <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+        
         {/* panel */}
         <div className="relative bg-white rounded-lg shadow-lg w-full max-w-md p-6">
+
           {/* boton de cerrar */}
           <button
             onClick={onClose}
             className="absolute top-3 right-3 text-gray-500 hover:text-gray-800 text-2xl leading-none"
           >
-            &times;
+            &times; {/* lo que hace es mostrar el icono "X" */}
           </button>
-  
+
           {/* titulo opcional, depende del boton */}
           {title && <h2 className="mb-4 text-xl font-semibold">{title}</h2>}
-  
+
           {/* Lo que se quiera pasar(agregar, editar o eliminar) */}
           {children}
         </div>

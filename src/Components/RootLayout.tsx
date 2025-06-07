d963c1751320325cbf81c984dddb7d2d37d3ecbd
ROSHIII96 <joseth.vargas.venegas@est.una.ac.cr>
2025-06-06 21:21:08 -0600

    Respaldo

--- src/Components/RootLayout.tsx
+++ src/Components/RootLayout.tsx
@@ -5,7 +5,7 @@ export const RootLayout = () => {
   return (
     <div className="min-h-screen flex flex-col w-full text-gray-800">
       <NavBar />
-    <main className="flex-grow container max-w-7xl mx-auto p-4 pt-18 pb-8">
+      <main className="flex-grow container max-w-7xl mx-auto p-4 pt-18 pb-8">
         <Outlet />
       </main>
     </div>
--- src/Pages/ListaReportesPage.tsx
+++ src/Pages/ListaReportesPage.tsx
@@ -8,10 +8,30 @@ import { useGetAverias } from ".././Hooks/useAverias";
 const ListaReportesPage = () => {
   const { data: averias, isLoading } = useGetAverias();
 
+  //Cuando isLoading es verdadero, muestra una animación de carga
   if (isLoading) {
     return (
-      <div className="pt-16">
-        <p className="text-center">Cargando...</p>
+      <div className="pt-16 flex justify-center items-center h-64">
+        <svg
+          className="animate-spin h-10 w-10 text-blue-700"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+            fill="none"
+          />
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+          />
+        </svg>
+        <p className="ml-4 text-blue-700 font-bold">Cargando...</p>
       </div>
     );
   }
